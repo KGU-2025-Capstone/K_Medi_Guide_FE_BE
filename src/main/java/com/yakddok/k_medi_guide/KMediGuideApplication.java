44052f84d470be00a86d226b694012420b7df8f8
EverJun2 <156490146+EverJun2@users.noreply.github.com>
2025-03-30 23:10:02 +0900

    init

---
+++ src/main/java/com/yakddok/k_medi_guide/KMediGuideApplication.java
@@ -0,0 +1,13 @@
+package com.yakddok.k_medi_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMediGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KMediGuideApplication.class, args);
+    }
+
+}
