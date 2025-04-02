f2e2c5f6d1cc0f66ab6a6f372df3d21762a6d16f
EverJun2 <deverjun2@gmail.com>
2025-04-02 14:52:45 +0900

    feat : GoogleMapServieImpl 파일 추가 (구글맵 비지니스로직)

---
+++ src/main/java/com/yakddok/k_medi_guide/service/impl/GoogleMapServiceImpl.java
@@ -0,0 +1,9 @@
+package com.yakddok.k_medi_guide.service.impl;
+
+import com.yakddok.k_medi_guide.dto.response.ResponsePhamarcyDTO;
+import com.yakddok.k_medi_guide.service.MapService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoogleMapServiceImpl implements MapService {
+}
