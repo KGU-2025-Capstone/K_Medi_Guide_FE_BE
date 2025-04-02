e1ebdb8499c598ab277eb7cc368ad19c725fe61d
EverJun2 <deverjun2@gmail.com>
2025-04-03 00:17:50 +0900

    feat : MapController 추가

--- src/main/java/com/yakddok/k_medi_guide/controller/MapController.java
+++ src/main/java/com/yakddok/k_medi_guide/controller/MapController.java
@@ -1,8 +1,34 @@
 package com.yakddok.k_medi_guide.controller;
 
-import org.springframework.stereotype.Controller;
+import com.yakddok.k_medi_guide.dto.response.ResponsePhamarcyDTO;
+import com.yakddok.k_medi_guide.service.MapService;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/map")
+@RequiredArgsConstructor
+
 public class MapController {
 
+    private final MapService mapService;
+
+    @PostMapping(value = "/getPhamarcy")
+    public ResponseEntity<ResponsePhamarcyDTO> getPhamarcy(
+        @RequestParam("latitude") BigDecimal latitude,
+        @RequestParam("longitude") BigDecimal longitude) {
+
+        ResponsePhamarcyDTO respDTO = mapService.getPhamarcy(latitude, longitude);
+
+        if(respDTO == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(respDTO);
+    }
 }
