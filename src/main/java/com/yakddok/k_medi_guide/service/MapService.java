82d0e9f4dec87fbdde96661f83a02ec7af99d217
EverJun2 <deverjun2@gmail.com>
2025-04-03 00:17:37 +0900

    feat : MapService 인터페이스 작성

--- src/main/java/com/yakddok/k_medi_guide/service/MapService.java
+++ src/main/java/com/yakddok/k_medi_guide/service/MapService.java
@@ -1,5 +1,10 @@
 package com.yakddok.k_medi_guide.service;
 
-public interface MapService {
+import com.yakddok.k_medi_guide.dto.response.ResponsePhamarcyDTO;
+import java.math.BigDecimal;
+import java.util.List;
+import org.springframework.http.HttpHeaders;
 
+public interface MapService {
+    public ResponsePhamarcyDTO getPhamarcy(BigDecimal latitude, BigDecimal longitude);
 }
