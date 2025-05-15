1572bbd27fbf9c1f31f1dbda9b6f77faa62410cb
skcy1515 <skcy151515@gmail.com>
2025-05-15 13:49:56 +0900

    FE 브랜치 통합, 약국 지도 기능 추가

--- src/main/java/com/yakddok/k_medi_guide/controller/MapController.java
+++ src/main/java/com/yakddok/k_medi_guide/controller/MapController.java
@@ -1,6 +1,6 @@
 package com.yakddok.k_medi_guide.controller;
 
-import com.yakddok.k_medi_guide.dto.response.ResponsePhamarcyDTO;
+import com.yakddok.k_medi_guide.dto.response.ResponsePharmacyDTO;
 import com.yakddok.k_medi_guide.service.MapService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -10,11 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/map")
@@ -24,8 +20,8 @@ public class MapController {
 
     private final MapService mapService;
 
-    @PostMapping(value = "/getPhamarcy")
-    @Operation(summary = "Get Pharmarcy list", description = "인근 약국 정보를 가져오는 API 입니다.")
+    @GetMapping(value = "/getPharmacy")
+    @Operation(summary = "Get Pharmacy list", description = "인근 약국 정보를 가져오는 API 입니다.")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "성공"),
         @ApiResponse(responseCode = "4xx", description = "실패")
@@ -34,11 +30,11 @@ public class MapController {
         @Parameter(name = "latitude", description = "latitude 좌표 값", example = "37.23414"),
         @Parameter(name = "longitude", description = "longitude 좌표 값", example = "125.12343")
     })
-    public ResponseEntity<ResponsePhamarcyDTO> getPhamarcy(
+    public ResponseEntity<ResponsePharmacyDTO> getPharmacy(
         @RequestParam("latitude") BigDecimal latitude,
         @RequestParam("longitude") BigDecimal longitude) {
 
-        ResponsePhamarcyDTO respDTO = mapService.getPhamarcy(latitude, longitude);
+        ResponsePharmacyDTO respDTO = mapService.getPharmacy(latitude, longitude);
 
         if(respDTO == null) {
             return ResponseEntity.notFound().build();
--- src/main/java/com/yakddok/k_medi_guide/dto/request/RequestPharmacyDTO.java
+++ src/main/java/com/yakddok/k_medi_guide/dto/request/RequestPharmacyDTO.java
@@ -1,14 +1,13 @@
 package com.yakddok.k_medi_guide.dto.request;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RequestPharmarcyDTO {
+public class RequestPharmacyDTO {
     private List<String> includedTypes;
     private int maxResultCount = 15;
     private LocationRestriction locationRestriction;
