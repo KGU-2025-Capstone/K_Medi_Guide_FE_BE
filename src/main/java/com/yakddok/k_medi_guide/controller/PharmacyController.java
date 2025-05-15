ef95bec339896acaf4783af58f18e788fdcd9a04
skcy1515 <skcy151515@gmail.com>
2025-05-15 13:49:56 +0900

    FE 브랜치 통합, 약국 지도 기능 추가

--- src/main/java/com/yakddok/k_medi_guide/controller/PharmacyController.java
+++ src/main/java/com/yakddok/k_medi_guide/controller/PharmacyController.java
@@ -1,36 +1,19 @@
 package com.yakddok.k_medi_guide.controller;
 
-import com.yakddok.k_medi_guide.dto.PharmacyDTO;
-import com.yakddok.k_medi_guide.service.impl.PharmacyService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/pharmacies")
 public class PharmacyController {
 
-    @Autowired
-    private PharmacyService pharmacyService;
+    @Value("${google.map.client-secret}")
+    private String googleMapApiKey;
 
-    @Value("${google.maps.api.key}")
-    private String googleMapsApiKey;
-
-    @GetMapping
-    public String showPharmaciesPage(Model model) {
-        model.addAttribute("googleMapsApiKey", googleMapsApiKey);
+    @GetMapping("/pharmacies")
+    public String showPharmacyTestPage(Model model) {
+        model.addAttribute("googleMapsApiKey", googleMapApiKey);
         return "pharmacies";
-    }
-
-    @GetMapping("/nearby")
-    @ResponseBody
-    public List<PharmacyDTO> getNearbyPharmacies(
-            @RequestParam double latitude,
-            @RequestParam double longitude) {
-        return pharmacyService.searchNearbyPharmacies(latitude, longitude);
     }
 }
