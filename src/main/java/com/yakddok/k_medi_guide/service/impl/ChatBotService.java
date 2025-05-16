594895b6995872643c23441eb875d15bf1de52a3
EverJun2 <deverjun2@gmail.com>
2025-05-17 01:03:12 +0900

    feat : ChatBotService 작성

--- src/main/java/com/yakddok/k_medi_guide/service/impl/ChatBotService.java
+++ src/main/java/com/yakddok/k_medi_guide/service/impl/ChatBotService.java
@@ -1,10 +1,14 @@
 package com.yakddok.k_medi_guide.service.impl;
 
-import com.yakddok.k_medi_guide.dto.ChatBotDTO;
+import com.yakddok.k_medi_guide.dto.response.ResponseChatBotDTO;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
@@ -13,25 +17,27 @@ import java.util.Map;
 public class ChatBotService {
 
     private final WebClient webClient;
+    private final Builder webClientBuilder;
 
-    public ChatBotService() {
+    public ChatBotService(
+        @Qualifier("webClientBuilder") Builder webClientBuilder) {
         this.webClient = WebClient.builder()
-                .baseUrl("http://localhost:5000") // Flask 서버 주소
-                .build();
+            .baseUrl("http://localhost:5000") // Flask 서버 주소
+            .build();
+        this.webClientBuilder = webClientBuilder;
     }
 
-    public Mono<ChatBotDTO> getMedicineSummary(String query) {
-        return webClient.post()
-                .uri("/medicine/summary")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(Map.of("query", query))
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, clientResponse ->
-                        clientResponse.bodyToMono(String.class).flatMap(errorBody ->
-                                Mono.error(new RuntimeException("에러 응답: " + errorBody))
-                        )
-                )
-                .bodyToMono(ChatBotDTO.class)
-                .doOnError(e -> System.out.println("에러 발생: " + e.getMessage()));
+    public ResponseEntity<ResponseChatBotDTO> send(String next, String input) {
+
+        ResponseChatBotDTO response = webClient.post()
+            .uri("api/medicine/" + next)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(input)
+            .retrieve()
+            .bodyToMono(ResponseChatBotDTO.class)
+            .block();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
 }
