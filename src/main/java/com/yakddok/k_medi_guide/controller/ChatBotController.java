065e9a71334db95e764e4e052ee4bbc6b22dc113
EverJun2 <deverjun2@gmail.com>
2025-05-17 14:15:23 +0900

    feat : ChatBotController 매개변수를 json으로 받도록 수정

--- src/main/java/com/yakddok/k_medi_guide/controller/ChatBotController.java
+++ src/main/java/com/yakddok/k_medi_guide/controller/ChatBotController.java
@@ -2,22 +2,25 @@ package com.yakddok.k_medi_guide.controller;
 
 import com.yakddok.k_medi_guide.dto.response.ResponseChatBotDTO;
 import com.yakddok.k_medi_guide.service.impl.ChatBotService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ChatBotController {
 
     private final ChatBotService chatBotService;
 
-    @PostMapping("/chatbot")
-    public ResponseEntity<ResponseChatBotDTO> sendChatBot(String next, String input) {
+    @PostMapping("/api/chatbot")
+    public ResponseEntity<ResponseChatBotDTO> sendChatBot(@RequestBody Map<String, String> json) {
+        String next = json.get("next");
+        String input = json.get("input");
+        System.out.println(next);
         try {
             ResponseEntity<ResponseChatBotDTO> response = chatBotService.send(next, input);
             return response;
